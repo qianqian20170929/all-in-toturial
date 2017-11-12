c0ca97234d56d982c4f6d6905c3fe190c79692aa
qianqian <qianqian20170929@gmail.com>
2017-11-12 15:28:44 +0800

    init finite-state-machine project

---
+++ finite-state-machine/src/main/java/com/hackx/FiniteStateMachineApplication.java
@@ -0,0 +1,12 @@
+package com.hackx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiniteStateMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiniteStateMachineApplication.class, args);
+	}
+}
