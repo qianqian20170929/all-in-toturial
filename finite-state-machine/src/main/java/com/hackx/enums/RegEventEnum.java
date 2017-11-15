a9e9ead154380012d2e9f80466ef18c9701a1c8b
qianqian <qianqian20170929@gmail.com>
2017-11-15 14:49:24 +0800

    add websocket demo

---
+++ finite-state-machine/src/main/java/com/hackx/enums/RegEventEnum.java
@@ -0,0 +1,20 @@
+package com.hackx.enums;
+
+/**
+ * Created by 曹磊(Hackx) on 12/11/2017.
+ * Email: caolei@mobike.com
+ */
+public enum RegEventEnum {
+
+    // 连接
+    CONNECT,
+    // 注册
+    REGISTER,
+    // 注册成功
+    REGISTER_SUCCESS,
+    // 注册失败
+    REGISTER_FAILED,
+    // 注销
+    UN_REGISTER;
+
+}
---
+++ finite-state-machine/src/main/java/com/hackx/enums/RegStatusEnum.java
@@ -0,0 +1,18 @@
+package com.hackx.enums;
+
+/**
+ * Created by 曹磊(Hackx) on 12/11/2017.
+ * Email: caolei@mobike.com
+ */
+public enum RegStatusEnum {
+
+    // 未连接
+    UNCONNECTED,
+    // 已连接
+    CONNECTED,
+    // 注册中
+    REGISTERING,
+    // 已注册
+    REGISTERED;
+
+}
